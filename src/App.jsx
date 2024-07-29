2ea33bea035b8d1adeee663a8a5038c6e18fe55b
Prasad-Abesinghe <lahiruprasad379@gmail.com>
2024-07-29 11:02:02 +0530

    create button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
   return (
     <>
       <h1>hello world</h1>
+      <ButtonGradient />
+      <div>
+        <Button> Heloo </Button>
+      </div>
     </>
   );
-}
+};
 
 export default App;
