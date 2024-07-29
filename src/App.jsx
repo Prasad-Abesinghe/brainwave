48b9f341e5bdbf71c3dbcca482f889abf4a6c46f
Prasad-Abesinghe <lahiruprasad379@gmail.com>
2024-07-29 11:38:49 +0530

    create header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1>hello world</h1>
-      <ButtonGradient />
-      <div>
-        <Button> Heloo </Button>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
       </div>
+      <ButtonGradient />
     </>
   );
 };
